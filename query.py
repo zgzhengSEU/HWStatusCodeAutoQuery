f576e1bd100a9dadb9d118d573c580fc41f08149
薛定谔的加菲猫 <62554593+zgzhengSEU@users.noreply.github.com>
2022-11-22 17:30:00 +0800

    Update query.py

--- query.py
+++ query.py
@@ -61,7 +61,7 @@ def queryStatus(hwuid, password, email, passwd, host_server, deviceFingerInfo):
     login_url = 'https://uniportal.huawei.com/uniportal/login.do'
     r = session.post(login_url, headers=headers, data=data, timeout=5)
     cur_time = datetime.now().timestamp()
-    print('\n 当前时间为：' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print('\n[', datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '] ', "执行一次查询")
     time = int(cur_time * 1000)
     url = 'https://career.huawei.com/reccampportal/services/portal/portaluser/queryMyJobInterviewPortal5?reqTime='
     url = url + str(time)
@@ -70,7 +70,7 @@ def queryStatus(hwuid, password, email, passwd, host_server, deviceFingerInfo):
     session.get(logout_url, timeout=5, headers=headers)  # 新增退出登录逻辑
     res = html_src.content.decode('utf-8')
     res_list = res.split('{')[1][:-2].split(',')
-    print("当前状态码为：\n")
+    print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ","当前状态码为：\n")
     for res in res_list:
         print(res)
     print()
@@ -100,87 +100,87 @@ if __name__ == "__main__":
     if "HWUID" in os.environ:
         hwuid = os.environ["HWUID"]
         if hwuid == '':
-            print("[", datetime.now(), "] ", "未找到 HWUID ！")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 HWUID ！")
             sys.exit(1) 
     else:
-        print("[", datetime.now(), "] ", "未找到 HWUID ！")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 HWUID ！")
         sys.exit(1)
 
     if "PASSWORD" in os.environ:
         password = os.environ["PASSWORD"]
         if password == '':
-            print("[", datetime.now(), "] ", "未找到 PASSWORD ！")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 PASSWORD ！")
             sys.exit(1) 
     else:
-        print("[", datetime.now(), "] ", "未找到 PASSWORD ！")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 PASSWORD ！")
         sys.exit(1)
 
     if "EMAIL" in os.environ:
         your_email = os.environ["EMAIL"]
         if your_email == '':
-            print("[", datetime.now(), "] ", "未找到 EMAIL ！")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 EMAIL ！")
             sys.exit(1) 
     else:
-        print("[", datetime.now(), "] ", "未找到 EMAIL ！")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 EMAIL ！")
         sys.exit(1)
 
     if "EMAILCODE" in os.environ:
         email_password = os.environ["EMAILCODE"]
         if email_password == '':
-            print("[", datetime.now(), "] ", "未找到 EMAILCODE ！")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 EMAILCODE ！")
             sys.exit(1) 
     else:
-        print("[", datetime.now(), "] ", "未找到 EMAILCODE ！")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未找到 EMAILCODE ！")
         sys.exit(1)
 
     if "SMTP" in os.environ:
         host_server = os.environ["SMTP"]
         if host_server == '':
-            print("[", datetime.now(), "] ", "未填写SMTP，默认使用QQ邮箱，SMTP 服务器为 smtp.qq.com")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未填写SMTP，默认使用QQ邮箱，SMTP 服务器为 smtp.qq.com")
             host_server = 'smtp.qq.com'            
         else:
-            print("[", datetime.now(), "] ", "设置 SMTP 服务器为：", host_server)
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "设置 SMTP 服务器为：", host_server)
     else:
-        print("[", datetime.now(), "] ", "未填写SMTP，默认使用QQ邮箱，SMTP 服务器为 smtp.qq.com")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "未填写SMTP，默认使用QQ邮箱，SMTP 服务器为 smtp.qq.com")
         host_server = 'smtp.qq.com'
 
     if "NOTIFY" in os.environ:
         notify = os.environ["NOTIFY"]
         if notify.lower() == "false":
             notify = False
-            print("[", datetime.now(), "] ", "关闭功能：每日定时启动脚本时，发送提醒脚本运行成功邮件")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "关闭功能：每日定时启动脚本时，发送提醒脚本运行成功邮件")
         else:
             notify = True
-            print("[", datetime.now(), "] ", "开启功能：每日定时启动脚本时，发送提醒脚本运行成功邮件")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "开启功能：每日定时启动脚本时，发送提醒脚本运行成功邮件")
     else:
         notify = True
-        print("[", datetime.now(), "] ", "开启功能：每日定时启动脚本时，发送提醒脚本运行成功邮件")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "开启功能：每日定时启动脚本时，发送提醒脚本运行成功邮件")
     
     if "DFI" in os.environ:
         deviceFingerInfo = os.environ["DFI"]
         if deviceFingerInfo == '':
-            print("[", datetime.now(), "] ", "请填写deviceFingerInfo，否则请检测是否运行成功，可能会出错")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "请填写deviceFingerInfo，否则请检测是否运行成功，可能会出错")
             deviceFingerInfo = 'bb84ac09e32b0ce23d488372c91a81d6'
     else:
-        print("[", datetime.now(), "] ", "请填写deviceFingerInfo，否则请检测是否运行成功，可能会出错")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "请填写deviceFingerInfo，否则请检测是否运行成功，可能会出错")
         deviceFingerInfo = 'bb84ac09e32b0ce23d488372c91a81d6'
     
     if len(sys.argv) == 2 and sys.argv[1] == 'test': # 添加快速测试
-        print("[", datetime.now(), "] ", "开始运行测试job")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "开始运行测试job")
         queryStatus(hwuid, password, your_email, email_password, host_server, deviceFingerInfo)
-        print("[", datetime.now(), "] ", "测试job结束，开始进入正式运行")
+        print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "测试job结束，开始进入正式运行")
         sys.exit(0)
     
     if notify:
         # 每天脚本定时运行时，发送启动成功提醒
         if len(sys.argv) == 2 and sys.argv[1] == 'start':
-            print("[", datetime.now(), "] ", "发送 每天脚本定时运行成功提醒")
+            print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "发送 每天脚本定时运行成功提醒")
             sendEmailStart(your_email, email_password, host_server)
 
     queryInterval = 1800  # 默认半小时查询一次
     start_time = datetime.now()
-    print("[", start_time, "] ", "启动当前job")
+    print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "启动当前job")
 
     work(hwuid, password, your_email, email_password, host_server, start_time, deviceFingerInfo)
 
-    print("[", datetime.now(), "] ", "当前job成功运行5小时，切换下一个job")
+    print("[", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "] ", "当前job成功运行5小时，切换下一个job")
